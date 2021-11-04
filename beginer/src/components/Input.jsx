a4d7d5e0fbee312e1228bdc69537392ed48d8aee
Binh <thanhbinh12369yt@gmail.com>
2021-11-04 16:39:51 +0700

    init

---
+++ beginer/src/components/Input.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Input({ type, onChange, value, ...props }) {
+	const handleChange = event => {
+		const value = event.target.value
+		if (type === 'number') {
+			if (/^\d+$/.text(value) || value === '') {
+				onChange(value)
+			}
+		} else {
+			onChange(value)
+		}
+	}
+
+	return (
+		<input
+			type={type === 'number' ? 'text' : type}
+			value={value}
+			onChange={handleChange}
+			{...props}
+		/>
+	)
+}
