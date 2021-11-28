bf586973b50e571c52316f544be11b8a0d00a51b
Binh <thanhbinh12369yt@gmail.com>
2021-11-28 09:04:17 +0700

    init

---
+++ yoloshop/.history/src/assets/fake-data/accessories_20211127125148.js
@@ -0,0 +1,4 @@
+const product_01_image_01 =
+	require('../images/accessory/accessory-01 (1).jpg').default
+const product_01_image_02 =
+	require('../images/products/product-01 (2).jpg').default
---
+++ yoloshop/.history/src/assets/fake-data/accessories_20211127125241.js
@@ -0,0 +1,4 @@
+const product_01_image_01 =
+	require('../images/accessory/accessory-1 (1).jpg').default
+const product_01_image_02 =
+	require('../images/accessory/accessory-1 (2).jpg').default
